013a0f1eaee959859cb731eabbebc7f19e940129
Mariusz-72 <mmbwdap@gmail.com>
2023-11-11 21:09:15 +0100

    update 3.1
    kilka zmian css

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -11,7 +11,7 @@ const Profile = ({ username, tag, location, avatar, stats: { followers, views, l
                     className={css.avatar}
             />
                 <p className={css.name}>{username}</p>
-                <p className={css.tag}>{tag}</p>
+                <p className={css.tag}>@{tag}</p>
                 <p className={css.location}>{location}</p>
         </div>
 
