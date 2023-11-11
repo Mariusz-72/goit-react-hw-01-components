edcaf655385adfd0770d5265d39f7ca83da95009
Mariusz-72 <mmbwdap@gmail.com>
2023-11-11 20:54:23 +0100

    update 3.0
    TransactionHistory

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,9 @@ import data from '../data/data.json';
 import FriendList from './FriendList/FriendList';
 import friends from '../data/friends.json';
 
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transactions.json';
+
 
 export const App = () => {
   return (
@@ -19,6 +22,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,12 +1,12 @@
 import css from './FriendList.module.css';
 import PropTypes from 'prop-types';
 
-const FriendListItem = ({ avatar, name, isOnline }) => {
-    const status = isOnline ? css.online : css.offline;
+export const FriendListItem = ({ avatar, name, isOnline }) => {
+    const statusClassName = isOnline ? css.online : css.offline;
 
     return (
         <li className={css.item}>
-            <span className={css.status}></span>
+            <span className={`${css.status} ${statusClassName}`}></span>
             <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
             <p className={css.name}>{name}</p>
         </li>
